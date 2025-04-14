2c29fe3a273e4814e9123c269e632f05a4c92513
stvshy <matisp6@op.pl>
2025-04-14 03:43:36 +0200

    Dalsze poprawki game.gd i ladder.gd

--- scripts/ladder.gd
+++ scripts/ladder.gd
@@ -4,9 +4,9 @@ signal entered_ladder
 signal exited_ladder
 
 func _on_body_entered(body):
-    if body.is_in_group("player"): # albo: if body == get_node("/root/Player") lub == self
-        entered_ladder.emit(body)
+	if body.is_in_group("player"): # albo: if body == get_node("/root/Player") lub == self
+		entered_ladder.emit(body)
 
 func _on_body_exited(body):
-    if body.is_in_group("player"):
-        exited_ladder.emit(body)
+	if body.is_in_group("player"):
+		exited_ladder.emit(body)
